ee2b9ba231a6400556b0fcdc18e40d57649c87c0
kiriakosv <kir.velissariou@gmail.com>
2014-04-02 18:17:20 +0300

    new exercises

---
+++ random_numbers.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+int myrandom(){
+	srand(time(NULL));
+	return (rand()%10);
+}
+
+int main(){
+	int number, input;
+	
+	number = myrandom();
+	do{
+		printf("\nGuess the number(0 to 10): ");
+		scanf("%d", &input);
+		if(input < number){
+			printf("Higher!\n");
+		}
+		if(input > number){
+			printf("Lower\n");
+		}
+	}while(number != input);
+	printf("Correct number!\n");
+	scanf("%d", input);
+	return 0;
+}
